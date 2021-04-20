595f23739684a140663a8a5fd4dc1ab37117ffc6
kyledean35 <73912971+kyledean35@users.noreply.github.com>
2021-04-20 16:34:54 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal('toggle');
+    });
+});
+
+
+$(document).ready(function(){
+    $("#loginButton").click(function(){
+        $("#loginModal").modal('toggle');
+    });
+});
+
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
